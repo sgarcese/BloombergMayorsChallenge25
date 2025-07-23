645034f31a72bf1ad3ca4e82b654f2fbb7c5cb68
sgarcese <sgarcese@gmail.com>
2025-07-23 09:48:46 -0500

    Update data.js to convert delays from hrs to secs

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const metrics = {
-  ttt: "Total Travel Time (hr)",
-  delay: "Total Delay (hr)",
+  ttt: "Total Travel Time (sec)",
+  delay: "Total Delay (sec)",
   avgDelay: "Avg Delay per Vehicle (sec)",
   stops: "Total Vehicle Stops",
   fuel: "Fuel Used (gal)",
@@ -12,22 +12,78 @@ const metrics = {
 const trafficData = {
   wvhs: {
     am: {
-      before: { ttt: 48.6, delay: 24.1, avgDelay: 68.1, stops: 2656, fuel: 34.1, hc: 596, co: 20637, nox: 1825 },
-      after:  { ttt: 48.5, delay: 22.9, avgDelay: 61.7, stops: 2749, fuel: 34.9, hc: 561, co: 20320, nox: 1758 }
+      before: { ttt: 48.6*3600, //convert to seconds
+               delay: 24.1*3600,
+               avgDelay: 68.1,
+               stops: 2656,
+               fuel: 34.1,
+               hc: 596,
+               co: 20637,
+               nox: 1825 },
+      after:  { ttt: 48.5*3600,
+               delay: 22.9*3600,
+               avgDelay: 61.7,
+               stops: 2749,
+               fuel: 34.9,
+               hc: 561,
+               co: 20320,
+               nox: 1758 }
     },
     pm: {
-      before: { ttt: 53.9, delay: 27.5, avgDelay: 63.0, stops: 2667, fuel: 38.7, hc: 680, co: 23755, nox: 2086 },
-      after:  { ttt: 56.4, delay: 28.5, avgDelay: 58.3, stops: 2924, fuel: 40.1, hc: 488, co: 21250, nox: 1672 }
+      before: { ttt: 53.9*3600,
+               delay: 27.5*3600,
+               avgDelay: 63.0,
+               stops: 2667,
+               fuel: 38.7,
+               hc: 680,
+               co: 23755,
+               nox: 2086 },
+      after:  { ttt: 56.4*3600,
+               delay: 28.5*3600,
+               avgDelay: 58.3,
+               stops: 2924,
+               fuel: 40.1,
+               hc: 488,
+               co: 21250,
+               nox: 1672 }
     }
   },
   hhs: {
     am: {
-      before: { ttt: 48.6, delay: 24.1, avgDelay: 68.1, stops: 2656, fuel: 34.1, hc: 596, co: 20637, nox: 1825 },
-      after:  { ttt: 48.5, delay: 22.9, avgDelay: 61.7, stops: 2749, fuel: 34.9, hc: 561, co: 20320, nox: 1758 }
+      before: { ttt: 48.6*3600,
+               delay: 24.1*3600,
+               avgDelay: 68.1,
+               stops: 2656,
+               fuel: 34.1,
+               hc: 596,
+               co: 20637,
+               nox: 1825 },
+      after:  { ttt: 48.5*3600,
+               delay: 22.9*3600,
+               avgDelay: 61.7,
+               stops: 2749,
+               fuel: 34.9,
+               hc: 561,
+               co: 20320,
+               nox: 1758 }
     },
     pm: {
-      before: { ttt: 53.9, delay: 27.5, avgDelay: 63.0, stops: 2667, fuel: 38.7, hc: 680, co: 23755, nox: 2086 },
-      after:  { ttt: 56.4, delay: 28.5, avgDelay: 58.3, stops: 2924, fuel: 40.1, hc: 488, co: 21250, nox: 1672 }
+      before: { ttt: 53.9*3600,
+               delay: 27.5*3600,
+               avgDelay: 63.0,
+               stops: 2667,
+               fuel: 38.7,
+               hc: 680,
+               co: 23755,
+               nox: 2086 },
+      after:  { ttt: 56.4*3600,
+               delay: 28.5*3600,
+               avgDelay: 58.3,
+               stops: 2924,
+               fuel: 40.1,
+               hc: 488,
+               co: 21250,
+               nox: 1672 }
     }
   }
 };
